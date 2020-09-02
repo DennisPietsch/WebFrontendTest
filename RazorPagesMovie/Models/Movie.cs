04b6b116a2bbe3fc00a282757ce32d9aeadda9eb
DennisPietsch <56264191+DennisPietsch@users.noreply.github.com>
2020-09-02 12:11:48 +0200

    .

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace RazorPagesMovie.Models
 {
@@ -11,9 +13,14 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        public decimal Price { get; set; }
+
+        [Column(TypeName ="decimal(18, 2)")]
+        public decimal Price { get; set; } 
+
+        public string Rating { get; set; }
     }
 }
