060fa2f8a89fa7efdcdbd0a768f4302d829216c9
DennisPietsch <56264191+DennisPietsch@users.noreply.github.com>
2020-09-03 08:20:12 +0200

    .

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -35,7 +35,8 @@ namespace RazorPagesMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "A"
                     },
 
                     new Movie
@@ -43,7 +44,8 @@ namespace RazorPagesMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "R"
                     },
 
                     new Movie
@@ -51,7 +53,8 @@ namespace RazorPagesMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "S"
                     }
                 ); ;
                 context.SaveChanges();
