ce563fd5a6beb77dace06820015ddf2aabcd53dc
DennisPietsch <56264191+DennisPietsch@users.noreply.github.com>
2020-09-29 16:26:30 +0200

    bugfixes, add error

--- RazorPagesMovie/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RazorPagesMovie/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,7 +83,7 @@ namespace RazorPagesMovie.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("User created a new account with password.");    
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
@@ -108,7 +108,8 @@ namespace RazorPagesMovie.Areas.Identity.Pages.Account
                 }
                 foreach (var error in result.Errors)
                 {
-                    ModelState.AddModelError(string.Empty, error.Description);
+                    return RedirectToPage("/Account/Error");
+                    //ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
 
